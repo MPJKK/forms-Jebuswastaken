e91641a2cabde33176dffd100445901488cc8b8c
Markus Vikholm <markuvik@ety-a2123b-13.edu.wks.metropolia.fi>
2018-02-06 10:55:20 +0200

    k

---
+++ src/app/logout/logout.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {MediaService} from '../services/media.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.scss'],
+})
+export class LogoutComponent implements OnInit {
+
+  constructor(private mediaService: MediaService, private router: Router) {
+  }
+
+  ngOnInit() {
+    localStorage.removeItem('token');
+    this.router.navigate(['login']);
+  }
+
+}
+
---
+++ src/app/models/login.ts
@@ -0,0 +1,5 @@
+export interface Login {
+  mesage: string;
+  token: string;
+  user: object;
+}
